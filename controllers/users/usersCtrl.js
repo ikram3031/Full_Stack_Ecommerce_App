928e88b1a4ca54127b0d38ae4e783b9bf56d6bf6
ikram3031 <mdikram.3031@gmail.com>
2022-08-23 17:53:51 +0600

    biroktikor

--- controllers/users/usersCtrl.js
+++ controllers/users/usersCtrl.js
@@ -1,4 +1,5 @@
 const User = require("../../model/user/User");
+const CustomError = require('../../errors')
 
 //-------------------------------------
 //Register
@@ -6,21 +7,20 @@ const User = require("../../model/user/User");
 
 const userRegisterCtrl = async (req, res) => {
   //Check if user Exist
-  const userExists = await User.findOne({ email: req?.body?.email });
+  const userExists = await User.findOne({ email: req.body.email });
 
-  if (userExists) throw new Error("User already exists");
-  try {
+  if (userExists) {
+    throw new CustomError.BadRequestError("Email Already Exists");
+  }
     //Register user
     const user = await User.create({
-      firstName: req?.body?.firstName,
-      lastName: req?.body?.lastName,
-      email: req?.body?.email,
-      password: req?.body?.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
     });
-    res.json(user);
-  } catch (error) {
-    res.json(error);
-  }
+
+    res.status(StatusCodes.CREATED).json({ user })
 };
 
 module.exports = { userRegisterCtrl };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ dotenv.config();
 //database
 const connectDB = require('./db/connect')
 
+//routes
 const userRoutes = require("./route/users/usersRoute");
 
 const app = express();
@@ -12,10 +13,16 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+//extra middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 //Users route
 app.use("/api/users", userRoutes);
 
-//server
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
 //port
 const port = process.env.PORT || 5000
 
@@ -23,7 +30,6 @@ const start = async () => {
     try{
         await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}...`))
-        console.log('Connected')
     } catch(error){
         console.log(error);
     }
