37c195a9dc90e20b9cf7e40c2d46df0b2c7ab2e0
ikram3031 <mdikram.3031@gmail.com>
2022-08-23 17:34:29 +0600

    2nd

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//database
+const connectDB = require('./db/connect')
+
+const userRoutes = require("./route/users/usersRoute");
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Users route
+app.use("/api/users", userRoutes);
+
+//server
+//port
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+        console.log('Connected')
+    } catch(error){
+        console.log(error);
+    }
+};
+
+start();
