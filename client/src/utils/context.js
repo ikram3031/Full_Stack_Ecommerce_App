322dd49df49730fb13908537b8e156cd6869b811
ikram3031 <mdikram.3031@gmail.com>
2023-01-15 23:51:13 +0600

    Creating Header

---
+++ client/src/utils/context.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+const Context = createContext();
+
+const AppContext = ({ children }) => {
+    return (
+        <Context.Provider>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default AppContext;
